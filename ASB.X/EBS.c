5091429517d055fa4979ac2d65914148caa9e780
93Urbano <davidjimenezurbano@gmail.com>
2022-08-02 11:32:11 +0200

    EBS LED y cambios WD

--- ASB.X/EBS.c
+++ ASB.X/EBS.c
@@ -20,6 +20,8 @@ void EBS_Init (void)
     MOSFET1_SetHigh();
     MOSFET2_SetHigh();
     EBS_Watchdog(WD_ENABLE);
+    EBSLEDACT_SetHigh();
+    AS_CLS_SDC_SetHigh();
 }
 
 void EBS_CheckUP_Routine (void)
@@ -154,8 +156,8 @@ void EBS_Watchdog (unsigned char ucWDState)
             PWM2_16BIT_Disable();
             break;
         case WD_ENABLE:
-            //PWM2_16BIT_Enable();
-            //GPIO_PWM2_Control(50, 100);
+            PWM2_16BIT_Enable();
+            GPIO_PWM2_Control(50, 30);
             break;
     }
 }
